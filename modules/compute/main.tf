8ecf0bf2ddc637eef7b41730c697847714defb13
Dhruv Barot <dbarot3553@gmail.com>
2026-05-27 15:21:13 -0400

    Fix : increase the EBS volume size to meet the requirement

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -93,7 +93,7 @@ resource "aws_launch_template" "main" {
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs {
-      volume_size           = 20
+      volume_size           = 100
       volume_type           = "gp2"
       encrypted = true
       delete_on_termination = true
